4fa264d243aa0c86f080f43d068bd3ec8001ac91
martinaglr <martaguilar@alumnos.uai.cl>
2025-03-19 13:14:47 -0300

    Se añade la estructura de los datos del archivo csv

--- App1.c
+++ App1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+
 #define _CRT_SECURE_NO_WARNINGS
 
 //Funcion encargada de Parsear el texto del csv, se debe ingresar el Num, el  cual es la
@@ -19,6 +21,140 @@ struct Registro{
    char pizza_ingredients;
    char pizza_name;
 };
+
+// Variables globales
+struct Registro *aRegistros; // Crear puntero para almacenar datos
+// Es un arreglo de iRegistros posiciones, cada una  del tipo Registro
+int iRegistros;              // Cantidad de registros del archivo
+char *sCabecera;             // Para salvar registro de cabecera
+
+//
+// Rutina para contar cantidad de registro de un archivo de texto
+//
+int CuentaRegistros(char* filename){
+   int iRegistros = 0;
+   FILE *file = fopen(filename, "r");
+   struct stat stStatusFile;  // Para capturar las característica del archivo, en particular propiedad st_size
+   stat(filename, &stStatusFile);
+   char *sRegistro = malloc(stStatusFile.st_size);  // Reservar memoria por el tamaño total (peor caso)
+
+   // Se recorre hasta EOF para contar
+   while (fscanf(file, "%[^\n] ", sRegistro) != EOF) {
+      //printf("Largo registro %i: %s\n", (int)strlen(sRegistro), sRegistro);
+      iRegistros++;
+   }
+   fclose(file);
+   iRegistros--; // Se resta uno para no considerar los encabezados
+   return iRegistros;
+}
+
+//
+// Rutina para cargar el archivo en el puntero de estructura Registro
+//
+void CargaRegistros(char* filename, int iRegistros)
+{
+   aRegistros = (struct Registro*)malloc(iRegistros * sizeof(struct Registro));
+   FILE* file = fopen(filename, "r");
+   struct stat stStatusFile;  // Para capturar las característica del archivo, en particular propiedad st_size
+   stat(filename, &stStatusFile);
+   char *sRegistro = malloc(stStatusFile.st_size);  // Reservar memoria por el tamaño total (peor caso)
+   int iPosRegistro = 0;
+
+   char *token;
+
+   // Salvamos el primer registro de cabecera
+   sCabecera = malloc(stStatusFile.st_size);
+   fscanf(file, "%[^\n] ", sCabecera);
+
+   // Se recorre hasta EOF para cargar
+   while (fscanf(file, "%[^\n] ", sRegistro) != EOF) {
+      //printf("%s\n", sRegistro);
+
+      // pizza_id
+      token = strtok(sRegistro, "\"");
+      //printf("libro: %s\n", token);
+      aRegistros[iPosRegistro].pizza_id = atoi(_strdup(token));
+
+      // coma, se ignora
+      token = strtok(NULL, "\"");
+      //printf("coma1: %s\n", token);
+
+      // order_id
+      token = strtok(NULL, "\"");
+      //printf("autor: %s\n", token);
+      aRegistros[iPosRegistro].order_id = atoi(_strdup(token));
+
+      // pizza_name_id
+      token = strtok(NULL, ",");
+      //printf("año: %s\n", token);
+      aRegistros[iPosRegistro].pizza_name_id = atoi(_strdup(token));
+
+      // quantity
+      token = strtok(NULL, ",");
+      //printf("estante: %s\n", token);
+      aRegistros[iPosRegistro].quantity = atoi(_strdup(token));
+
+      // order_date
+      token = strtok(NULL, "\"");
+      //printf("sección: %s\n", token);
+      aRegistros[iPosRegistro].order_date = atoi(_strdup(token));
+
+      // order_time
+      token = strtok(NULL, ",");
+      //printf("piso: %s\n", token);
+      aRegistros[iPosRegistro].order_time = atoi(_strdup(token));
+
+      // unit_price
+      token = strtok(NULL, "\"");
+      //printf("edificio: %s\n", token);
+      aRegistros[iPosRegistro].unit_price = atoi(_strdup(token));
+
+      // coma, se ignora
+      token = strtok(NULL, "\"");
+      //printf("coma: %s\n", token);
+
+      // total_price
+      token = strtok(NULL, "\"");
+      //printf("sede: %s\n", token);
+      aRegistros[iPosRegistro].total_price = atoi(_strdup(token));
+
+      // pizza_size
+      token = strtok(NULL, "\"");
+      //printf("sede: %s\n", token);
+      aRegistros[iPosRegistro].pizza_size = atoi(_strdup(token));
+
+
+      // pizza_category
+      token = strtok(NULL, "\"");
+      //printf("sede: %s\n", token);
+      aRegistros[iPosRegistro].pizza_category = atoi(_strdup(token));
+
+
+      // total_price
+      token = strtok(NULL, "\"");
+      //printf("sede: %s\n", token);
+      aRegistros[iPosRegistro].total_price = atoi(_strdup(token));
+
+
+      // pizza_ingredients
+      token = strtok(NULL, "\"");
+      //printf("sede: %s\n", token);
+      aRegistros[iPosRegistro].pizza_ingredients = atoi(_strdup(token));
+
+
+      // pizza_name
+      token = strtok(NULL, "\"");
+      //printf("sede: %s\n", token);
+      aRegistros[iPosRegistro].pizza_name = atoi(_strdup(token));
+
+
+
+      iPosRegistro++;
+   }
+   fclose(file);
+   return;
+
+}
 
 
 //Funcion main la cual debe tener la funcionaliad de leer el archivo csv
