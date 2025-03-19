f7f1caec28e9906cf50675a07cc075c4c3a8735d
martinaglr <martaguilar@alumnos.uai.cl>
2025-03-19 11:50:14 -0300

    Create Aplicacion.c
    Se añade un tipo de estructura para guiarse

---
+++ Aplicacion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//Funcion main la cual debe tener la funcionaliad de leer el archivo csv
+//Funcion para decidir el valor mas repetido/menos repetido (Pizza mas y
+//menos vendida)
+
+//Funcion para extraer la fecha con mas y menos ganancias junto a la cantidad
+
+//Fechas con mas y menos ventas en terminos de cantidad junto a la cantidad
+
+//Funcion para calcular el promedio de una columna (promedio pizzas por orden y dia)
+
+//Funcion para obtener el mayor valor dentro de una columna (Ingrediente mas vendido)
+
+//Funcion para sumar la cant de pizzas por categoria
+
