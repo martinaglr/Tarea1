0fa370b3ed556ca38e7d93f31516d30efbc15bec
martinaglr <martaguilar@alumnos.uai.cl>
2025-03-19 12:55:18 -0300

    Se añade la estructura de los datos del archivo csv

--- App1.c
+++ App1.c
@@ -1,4 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+//Funcion encargada de Parsear el texto del csv, se debe ingresar el Num, el  cual es la
+//columna de informacion que se desea extraer
+struct Registro{
+   int pizza_id;
+   int order_id;
+   char pizza_name_id;
+   int quantity;
+   char order_date;
+   char order_time;
+   int unit_price;
+   int total_price;
+   char pizza_size;
+   char pizza_category;
+   char pizza_ingredients;
+   char pizza_name;
+};
+
+
 //Funcion main la cual debe tener la funcionaliad de leer el archivo csv
 //Funcion para decidir el valor mas repetido/menos repetido (Pizza mas y
 //menos vendida)
