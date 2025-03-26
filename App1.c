8b87675ae0f8346a9603782645fb7d581468df5f
martinaglr <martaguilar@alumnos.uai.cl>
2025-03-26 12:39:45 -0300

    Se termino la funcionalidad para abrir el archivo, leerlo, mostrar los datos en pantalla,y almacenarlos en un puntero

--- App1.c
+++ App1.c
@@ -7,156 +7,186 @@
 
 //Funcion encargada de Parsear el texto del csv, se debe ingresar el Num, el  cual es la
 //columna de informacion que se desea extraer
-struct Registro{
-   int pizza_id;
-   int order_id;
-   char pizza_name_id;
-   int quantity;
-   char order_date;
-   char order_time;
-   int unit_price;
-   int total_price;
-   char pizza_size;
-   char pizza_category;
-   char pizza_ingredients;
-   char pizza_name;
+char NombreArchivo[80];
+
+struct Registro {
+    int pizza_id;
+    int order_id;
+    char* pizza_name_id;
+    int quantity;
+    char* order_date;
+    char* order_time;
+    int unit_price;
+    int total_price;
+    char* pizza_size;
+    char* pizza_category;
+    char* pizza_ingredients;
+    char* pizza_name;
 };
 
 // Variables globales
 struct Registro *aRegistros; // Crear puntero para almacenar datos
-// Es un arreglo de iRegistros posiciones, cada una  del tipo Registro
+// Es un arreglo de iRegistros posiciones, cada una del tipo Registro
 int iRegistros;              // Cantidad de registros del archivo
 char *sCabecera;             // Para salvar registro de cabecera
 
-//
 // Rutina para contar cantidad de registro de un archivo de texto
-//
-int CuentaRegistros(char* filename){
-   int iRegistros = 0;
-   FILE *file = fopen(filename, "r");
-   struct stat stStatusFile;  // Para capturar las característica del archivo, en particular propiedad st_size
-   stat(filename, &stStatusFile);
-   char *sRegistro = malloc(stStatusFile.st_size);  // Reservar memoria por el tamaño total (peor caso)
+int CuentaRegistros(char* filename) {
+    int iRegistros = 0;
+    FILE *file = fopen(filename, "r");
+    struct stat stStatusFile;  // Para capturar las características del archivo, en particular propiedad st_size
+    stat(filename, &stStatusFile);
+    char *sRegistro = malloc(stStatusFile.st_size);  // Reservar memoria por el tamaño total (peor caso)
 
-   // Se recorre hasta EOF para contar
-   while (fscanf(file, "%[^\n] ", sRegistro) != EOF) {
-      //printf("Largo registro %i: %s\n", (int)strlen(sRegistro), sRegistro);
-      iRegistros++;
-   }
-   fclose(file);
-   iRegistros--; // Se resta uno para no considerar los encabezados
-   return iRegistros;
+    // Se recorre hasta EOF para contar
+    while (fscanf(file, "%[^\n] ", sRegistro) != EOF) {
+        iRegistros++;
+    }
+    fclose(file);
+    iRegistros--; // Se resta uno para no considerar los encabezados
+    return iRegistros;
 }
 
-//
 // Rutina para cargar el archivo en el puntero de estructura Registro
-//
-void CargaRegistros(char* filename, int iRegistros)
-{
-   aRegistros = (struct Registro*)malloc(iRegistros * sizeof(struct Registro));
-   FILE* file = fopen(filename, "r");
-   struct stat stStatusFile;  // Para capturar las característica del archivo, en particular propiedad st_size
-   stat(filename, &stStatusFile);
-   char *sRegistro = malloc(stStatusFile.st_size);  // Reservar memoria por el tamaño total (peor caso)
-   int iPosRegistro = 0;
+void CargaRegistros(char* filename, int iRegistros) {
+    aRegistros = (struct Registro*)malloc(iRegistros * sizeof(struct Registro));
+    FILE* file = fopen(filename, "r");
+    if (!file) {
+        printf("No se pudo abrir el archivo");
+        return;
+    }
+    struct stat stStatusFile;  // Para capturar las características del archivo, en particular propiedad st_size
+    stat(filename, &stStatusFile);
+    char *sRegistro = malloc(stStatusFile.st_size);  // Reservar memoria por el tamaño total (peor caso)
+    int iPosRegistro = 0;
 
-   char *token;
+    char *token;
 
-   // Salvamos el primer registro de cabecera
-   sCabecera = malloc(stStatusFile.st_size);
-   fscanf(file, "%[^\n] ", sCabecera);
+    // Salvamos el primer registro de cabecera
+    sCabecera = malloc(stStatusFile.st_size);
+    fgets(sCabecera, stStatusFile.st_size, file);
 
-   // Se recorre hasta EOF para cargar
-   while (fscanf(file, "%[^\n] ", sRegistro) != EOF) {
-      //printf("%s\n", sRegistro);
+    // Se recorre hasta EOF para cargar
+    while (fgets(sRegistro, stStatusFile.st_size, file)) {
+        // Remover el salto de línea si lo hay
+        sRegistro[strcspn(sRegistro, "\n")] = 0;
 
-      // pizza_id
-      token = strtok(sRegistro, "\"");
-      //printf("libro: %s\n", token);
-      aRegistros[iPosRegistro].pizza_id = atoi(_strdup(token));
+        // pizza_id
+        token = strtok(sRegistro, ",");
+        if (token) {
+            aRegistros[iPosRegistro].pizza_id = atoi(token);
+        }
 
-      // coma, se ignora
-      token = strtok(NULL, "\"");
-      //printf("coma1: %s\n", token);
+        // order_id
+        token = strtok(NULL, ",");
+        if (token) {
+            aRegistros[iPosRegistro].order_id = atoi(token);
+        }
 
-      // order_id
-      token = strtok(NULL, "\"");
-      //printf("autor: %s\n", token);
-      aRegistros[iPosRegistro].order_id = atoi(_strdup(token));
+        // pizza_name_id
+        token = strtok(NULL, ",");
+        if (token) {
+            aRegistros[iPosRegistro].pizza_name_id = strdup(token);
+        }
 
-      // pizza_name_id
-      token = strtok(NULL, ",");
-      //printf("año: %s\n", token);
-      aRegistros[iPosRegistro].pizza_name_id = atoi(_strdup(token));
+        // quantity
+        token = strtok(NULL, ",");
+        if (token) {
+            aRegistros[iPosRegistro].quantity = atoi(token);
+        }
 
-      // quantity
-      token = strtok(NULL, ",");
-      //printf("estante: %s\n", token);
-      aRegistros[iPosRegistro].quantity = atoi(_strdup(token));
+        // order_date
+        token = strtok(NULL, ",");
+        if (token) {
+            aRegistros[iPosRegistro].order_date = strdup(token);
+        }
 
-      // order_date
-      token = strtok(NULL, "\"");
-      //printf("sección: %s\n", token);
-      aRegistros[iPosRegistro].order_date = atoi(_strdup(token));
+        // order_time
+        token = strtok(NULL, ",");
+        if (token) {
+            aRegistros[iPosRegistro].order_time = strdup(token);
+        }
 
-      // order_time
-      token = strtok(NULL, ",");
-      //printf("piso: %s\n", token);
-      aRegistros[iPosRegistro].order_time = atoi(_strdup(token));
+        // unit_price
+        token = strtok(NULL, ",");
+        if (token) {
+            aRegistros[iPosRegistro].unit_price = atoi(token);
+        }
 
-      // unit_price
-      token = strtok(NULL, "\"");
-      //printf("edificio: %s\n", token);
-      aRegistros[iPosRegistro].unit_price = atoi(_strdup(token));
+        // total_price
+        token = strtok(NULL, ",");
+        if (token) {
+            aRegistros[iPosRegistro].total_price = atoi(token);
+        }
 
-      // coma, se ignora
-      token = strtok(NULL, "\"");
-      //printf("coma: %s\n", token);
+        // pizza_size
+        token = strtok(NULL, ",");
+        if (token) {
+            aRegistros[iPosRegistro].pizza_size = strdup(token);
+        }
 
-      // total_price
-      token = strtok(NULL, "\"");
-      //printf("sede: %s\n", token);
-      aRegistros[iPosRegistro].total_price = atoi(_strdup(token));
+        // pizza_category
+        token = strtok(NULL, ",");
+        if (token) {
+            aRegistros[iPosRegistro].pizza_category = strdup(token);
+        }
 
-      // pizza_size
-      token = strtok(NULL, "\"");
-      //printf("sede: %s\n", token);
-      aRegistros[iPosRegistro].pizza_size = atoi(_strdup(token));
+        // pizza_ingredients
+        token = strtok(NULL, ",");
+        if (token) {
+            aRegistros[iPosRegistro].pizza_ingredients = strdup(token);
+        }
 
+        // pizza_name
+        token = strtok(NULL, ",");
+        if (token) {
+            aRegistros[iPosRegistro].pizza_name = strdup(token);
+        }
 
-      // pizza_category
-      token = strtok(NULL, "\"");
-      //printf("sede: %s\n", token);
-      aRegistros[iPosRegistro].pizza_category = atoi(_strdup(token));
-
-
-      // total_price
-      token = strtok(NULL, "\"");
-      //printf("sede: %s\n", token);
-      aRegistros[iPosRegistro].total_price = atoi(_strdup(token));
-
-
-      // pizza_ingredients
-      token = strtok(NULL, "\"");
-      //printf("sede: %s\n", token);
-      aRegistros[iPosRegistro].pizza_ingredients = atoi(_strdup(token));
-
-
-      // pizza_name
-      token = strtok(NULL, "\"");
-      //printf("sede: %s\n", token);
-      aRegistros[iPosRegistro].pizza_name = atoi(_strdup(token));
-
-
-
-      iPosRegistro++;
-   }
-   fclose(file);
-   return;
-
+        iPosRegistro++;
+    }
+    fclose(file);
+    return;
 }
 
+void MostrarDatos() {
+    printf("Reg. 0: cabecera: %s\n", sCabecera);
+    for (int iPosRegistro = 0; iPosRegistro < iRegistros; iPosRegistro++) {
+        printf("Reg. %i: %i | %i | %s | %i | %s | %s | %i | %i | %s | %s | %s | %s|\n",
+            iPosRegistro + 1,
+            aRegistros[iPosRegistro].pizza_id,
+            aRegistros[iPosRegistro].order_id,
+            aRegistros[iPosRegistro].pizza_name_id,
+            aRegistros[iPosRegistro].quantity,
+            aRegistros[iPosRegistro].order_date,
+            aRegistros[iPosRegistro].order_time,
+            aRegistros[iPosRegistro].unit_price,
+            aRegistros[iPosRegistro].total_price,
+            aRegistros[iPosRegistro].pizza_size,
+            aRegistros[iPosRegistro].pizza_category,
+            aRegistros[iPosRegistro].pizza_ingredients,
+            aRegistros[iPosRegistro].pizza_name);
+    }
+    return;
+}
 
+void orchestrator() {
+    char *filename = NombreArchivo;
+    printf("Debug:Nombre recibido de archivo es: %s\n", filename);
+    iRegistros = CuentaRegistros(filename);
+    printf("Cantidad de registros: %i\n\n", iRegistros);
+    CargaRegistros(filename, iRegistros);
+    MostrarDatos();
+    return;
+}
+
+int main(void) {
+    printf("Ingrese el nombre del archivo CSV: ");
+    fgets(NombreArchivo, sizeof(NombreArchivo), stdin);
+    NombreArchivo[strcspn(NombreArchivo, "\n")] = '\0';
+    orchestrator();
+    return 0;
+}
 //Funcion main la cual debe tener la funcionaliad de leer el archivo csv
 //Funcion para decidir el valor mas repetido/menos repetido (Pizza mas y
 //menos vendida)
